e619eeeeeef1b8afe870b3ab71678728907cf5e9
Mohan Kumar <mohankumar@Mohans-MacBook-Pro.local>
2017-11-05 23:03:55 +0530

    lesson1 adding file and creating server to server static files

---
+++ app-server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist'));
+
+app.listen('3000');
+console.log("listing to http://localhost:3000");
