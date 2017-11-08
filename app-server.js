3e1ba50d08ce0330ff6ffc16eafdc5817352a559
Mohan Kumar <mohankumar@Mohans-MacBook-Pro.local>
2017-11-08 23:40:28 +0530

    lesson2 adding web pack support and load first react app using bundle.js

--- app-server.js
+++ app-server.js
@@ -1,8 +1,9 @@
 var express = require('express');
+
 var app = express();
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen('3000');
-console.log("listing to http://localhost:3000");
+app.listen(3000);
+console.log("Polling server is running at 'http://localhost:3000'");
