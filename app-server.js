501279606fdfb03f70426d73a0f6a5a27fe0cab2
Mohan Kumar <mohankumar@Mohans-MacBook-Pro.local>
2017-11-15 09:55:55 +0530

    lesson 6 adding state variable

--- app-server.js
+++ app-server.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+var connections = [];
+
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
@@ -9,6 +11,14 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection',function(socket){
-    console.log("socket connected " , socket.id);
+
+    socket.once('disconnect',function () {
+
+        connections.splice(connections.indexOf(socket),1);
+        socket.disconnect();
+        console.log("disconnection" + connections.length);
+    })
+    connections.push(socket);
+    console.log("socket connected " , connections.length);
 })
 console.log("Polling server is running at 'http://localhost:3000'");
--- components/APP.js
+++ components/APP.js
@@ -2,17 +2,28 @@ var React = require('react');
 var io = require('socket.io-client');
 var Header = require('./parts/header');
 var APP = React.createClass({
+	getInitialState(){
+		return {
+			status:"disconnected"
+		}
+	},
 	componentWillMount (){
 		this.socket = io('http://localhost:3000');
 		this.socket.on('connect',this.connect);
+		this.socket.on('disconnect',this.disconnect);
 	},
 	connect (){
+		this.setState({status:"connected"});
 		console.log("connect " + this.socket.id);
+	},
+    disconnect (){
+		this.setState({status:"disconnected"});
+		console.log("disconnect " + this.socket.id);
 	},
 	render() {
 		return (
 			<div>
-				<Header title="new header"/>
+				<Header title="new header" status={this.state.status}/>
 			</div>
 
 		);
